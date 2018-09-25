5d469a7b6c226f49945bf0ba19873d20e1c7384e
ErwanEDEN <erwanzimmer@gmail.com>
2018-09-25 10:27:47 +0200

    Merci Patrick la trique

---
+++ WeatherApp/WeatherApp/WeatherApp/WeatherPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace WeatherApp
+{
+    public partial class WeatherPage : ContentPage
+    {
+        public WeatherPage()
+        {
+            InitializeComponent();
+
+            //Set the default binding to a default object for now
+            BindingContext = new Weather();
+        }
+
+        private async void GetWeatherBtn_Clicked(object sender, EventArgs e)
+        {
+            Weather weather = await Core.GetWeather("60601");
+            getWeatherBtn.Text = weather.Title;
+        }
+    }
+}
